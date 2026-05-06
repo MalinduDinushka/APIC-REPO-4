a7db06e599a13fb42b2d19edc5708ab09de85b96
MalinduDinushka <mdhherath@gmail.com>
2026-05-06 11:02:51 +0530

    new 2

--- scripts/login.bat
+++ scripts/login.bat
@@ -1,2 +1,3 @@
 C:\ApiCLI\toolkit-windows\apic client-creds:set credentials.json
-C:\ApiCLI\toolkit-windows\apic login --sso --context provider --server https://platform-api.us-east-a.apiconnect.automation.ibm.com --apiKey 5441f211-daae-4619-82a9-3b816b6556a4
+C:\ApiCLI\toolkit-windows\apic login --sso --context provider --server https://platform-api.trial.apiconnect.automation.ibm.com --apiKey a509f344-cc3d-4efe-90de-00647b63ae77
+
