4315b3bd82f24245aab8bf5c8d011cfd17feab5a
MalinduDinushka <mdhherath@gmail.com>
2026-05-06 11:24:21 +0530

    new4

--- scripts/login.bat
+++ scripts/login.bat
@@ -1,3 +1,6 @@
-C:\ApiCLI\toolkit-windows\apic client-creds:set credentials.json
-C:\ApiCLI\toolkit-windows\apic login --sso --context provider --server https://platform-api.trial.apiconnect.automation.ibm.com --apiKey a509f344-cc3d-4efe-90de-00647b63ae77
+@echo off
+set APIC_ACCEPT_LICENSE=true
 
+C:\ApiCLI\toolkit-windows\apic client-creds:set credentials.json 
+
+C:\ApiCLI\toolkit-windows\apic login --sso --context provider --server https://platform-api.trial.apiconnect.automation.ibm.com --apiKey a509f344-cc3d-4efe-90de-00647b63ae77
